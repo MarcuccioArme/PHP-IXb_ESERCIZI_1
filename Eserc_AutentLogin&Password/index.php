9f20ba557afbfe812094eb1fadae5f564e8ea928
Marco Armenise <armenise.marco@panettipitagora.edu.it>
2024-01-22 21:36:19 +0100

    Aggiunti file
    Aggiunta Pagina 1:
    index.php
    
    Aggiunta Pagina 2:
    controllo.php
    (da configurare)

---
+++ Eserc_AutentLogin&Password/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+
+    <h1>Login</h1>
+
+    <form action="controllo.php" method="post">
+
+        Username: <input type="text" name="username" required> <br><br>
+        Password: <input type="password" name="password" required>
+
+        <input type="hidden" name="provenienza" value="1"> <br><br>
+
+        <input type="submit" name="login" value="LOGIN">
+        <input type="submit" name="registrati" value="REGISTRATI">
+
+    </form>
+    
+</body>
+</html>
